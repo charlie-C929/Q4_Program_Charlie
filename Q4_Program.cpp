deb33b7d9c0e9a1d22d2da0e211da6fca1c4c86c
MaxZZG <zzgdar@163.com>
2020-10-03 15:16:57 +0800

    第一次提交，测试用

---
+++ Q4_Program.cpp
@@ -0,0 +1,15 @@
+﻿
+/*
+        程序作者：Max_Zhang
+        程序功能：等参单元四节点，Q4_Program 主程序
+*/
+
+#include <iostream>
+#include <vector>
+#include "ex1.hpp"
+
+int main(int argc , char* argv[])
+{
+    ex1();
+}
+
