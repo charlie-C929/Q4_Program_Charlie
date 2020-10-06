1eb366a2db7f24938363688db8fbfb5f140e2226
MaxZZG <zzgdar@163.com>
2020-10-06 21:06:23 +0800

    添加sln文件

---
+++ Q4_Program/Q4_Program.cpp
@@ -0,0 +1,20 @@
+﻿
+/*
+        程序作者：Max_Zhang
+        程序功能：等参单元四节点，Q4_Program 主程序
+*/
+
+#include <iostream>
+#include <vector>
+#include "ex1.hpp"
+#include "shapeFuncAndDiff.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    ex1();
+    double s = 0.5, t = 0.5;
+    double *N = shapeFunc(s, t);
+    cout << N[0] << N[1] << N[2] << N[3] << endl;
+}
--- example/ex1.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef EX1_HPP
-#define EX1_HPP
-
-int ex1();
-
-
-#endif // !EX1_HPP
-
